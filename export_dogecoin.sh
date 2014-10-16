00737be1aca593caa8ade66decfb6aacbef8009e
Simon de la Rouviere <simon@delarouviere.com>
2014-10-16 17:35:25 +0200

    quick fix

---
+++ export_dogecoin.sh
@@ -0,0 +1,9 @@
+#/!/bin/sh
+
+export BITCOIND_HOST=127.0.0.1
+export BITCOIND_DATADIR=~/Library/Application\ Support/Dogecoin/
+export BITCOIND_PORT=22555
+export BITCOIND_P2P_PORT=22556
+export INSIGHT_NETWORK=livenet
+
+#export user/pass self
